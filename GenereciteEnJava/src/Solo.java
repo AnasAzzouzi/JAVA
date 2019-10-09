4dc530dc6c526f03651136459f9903eacae20df3
AnasAzzouzi <haitham.azzouzi@gmail.com>
2019-10-09 09:46:27 +0100

    java

--- GenereciteEnJava/src/Solo.java
+++ GenereciteEnJava/src/Solo.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 public class Solo <T>{
 
 	private T valeur;
@@ -18,7 +21,7 @@ public class Solo <T>{
 		return ""+this.valeur;
 	}
 	public static void main(String[] args) {
-		Solo<Integer> s1= new Solo<Integer>(123);
+		/*Solo<Integer> s1= new Solo<Integer>(123);
 		Solo<String> s2= new Solo<String>("Hello");
 		Solo<Double> s3= new Solo<Double>(12358.54);
 		System.out.println(s1);
@@ -34,12 +37,31 @@ public class Solo <T>{
 		list.add("6");
 		Iterator it = list.iterator();
 		while(it.hasNext()) {
-			String str=(String)it.next();
+			/*String str=(String)it.next();
+			if(str.equals("4")|| str.equals("2"))
+			it.next();
+			it.remove();
 			
-			if(str.equals("4")|| str.equals("2")) it.remove();
-			System.out.println(str);
+			System.out.println("removed");
 		}
-
+		System.out.println(list);*/
+		Map<Integer,String> map=new HashMap<Integer,String>(); 
+		map.put(1, "un");
+		map.put(2, "deux");
+		map.put(3, "trois");
+		map.put(4, "quatre");
+		map.put(5, "cinq");
+		map.put(6, "six");
+		Set keys=map.keySet();
+		Iterator it= keys.iterator();
+		while(it.hasNext()) {
+			Integer k=(Integer)it.next();
+			
+			System.out.println(" key  = "+k+" value = "+map.get(k));
+			
+		}
+		
+		
 
 	
 		
