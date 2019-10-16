94a738a417c7fd2127bdaa5f7e8ec49b87ff7ed3
AnasAzzouzi <haitham.azzouzi@gmail.com>
2019-10-16 09:39:59 +0100

    v2

---
+++ GenereciteEnJava/src/CD.java
@@ -0,0 +1,59 @@
+
+public class CD implements Comparable<CD> {
+	private String auteur,titre;
+	private double prix;
+
+
+	public CD(String auteur,String titre,double prix) {
+		
+		this.auteur=auteur;
+		this.titre=titre;
+		this.prix=prix;
+	}
+
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+
+	public String getTitre() {
+		return titre;
+	}
+
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+
+	public double getPrix() {
+		return prix;
+	}
+
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+
+	@Override
+	public String toString() {
+		return "CD [auteur=" + auteur + ", titre=" + titre + ", prix=" + prix + "]";
+	}
+
+
+	@Override
+	public int compareTo(CD C) {
+		// TODO Auto-generated method stub
+		return(int)(C.prix-this.prix);
+	}
+	
+	
+
+}
--- GenereciteEnJava/src/Trie.java
+++ GenereciteEnJava/src/Trie.java
@@ -1,13 +1,15 @@
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class Trie {
 
 	public static void main(String[] args) {
-		Set<String> tree = new TreeSet<String>();
+		/*Set<String> tree = new TreeSet<String>();
 		tree.add("Halima");
-		tree.add("Mbarka");
+		tree.add("Malika");
 		tree.add("Malika");
 		tree.add("Arbi");
 		tree.add("Moustafa");
@@ -15,8 +17,25 @@ public class Trie {
 		tree.add("El Mekki");
 		tree.add("Rayan");
 		Iterator<String> it=tree.iterator();
-		while(it.hasNext()) System.out.println(it.next());
+		while(it.hasNext()) System.out.println(it.next());*/
+		
+		CD C =new CD("anas","test",120);
+		CD C2 =new CD("zak","test2",20);
+		CD C3 =new CD("zak","test2",50);
+		CD C4 =new CD("zak","test2",80);
+
+		
+		ArrayList<CD> list= new ArrayList<CD>();
+		list.add(C2);
+		list.add(C);
+		list.add(C3);
+		list.add(C4);
 			
+		Collections.sort(list);
+		Iterator it = list.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next().toString());
+		}
 
 		
 
