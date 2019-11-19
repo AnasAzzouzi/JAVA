443b99d490791800aa03e68d34765c306ccf4342
AnasAzzouzi <haitham.azzouzi@gmail.com>
2019-11-19 09:09:41 +0000

    tp2JAVA..v1

---
+++ ExempleCours/src/TP2JAVA/Compteur.java
@@ -0,0 +1,11 @@
+package TP2JAVA;
+
+public class Compteur{
+    String nom;
+    int maxValue;
+
+    public Compteur(String nom, int maxValue) {
+        this.nom = nom;
+        this.maxValue = maxValue;
+    }
+}
